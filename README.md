de68ad6ba7593e9ddcd5478e6228678357d208e3
totolo7942@gmail.com <Sung0630&1>
2021-04-21 13:11:17 +0900

    init:

---
+++ README.md
@@ -0,0 +1,5 @@
+# gradle-multi-module
+
+Gradle 5.x + Spring Boot 2.2.x 관련 각 기본 라이브러리 최신 빌드 유지.
+
+빌드 껍데기이며(정상 빌드만 확인), 실제 테스트는 grissom-mart 에서 진행한다.
---
+++ core/src/main/java/com/tistory/eclipse4j/backend/dispcategory/entity/DispCategory.java
@@ -0,0 +1,13 @@
+package com.tistory.eclipse4j.backend.dispcategory.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class DispCategory {
+
+    @Id
+    private Long id;
+
+
+}
